8399c45260bb991cb5c540c6331c0876b0805d62
jyajoo <jyajoo1020@gmail.com>
2022-10-18 09:23:40 +0900

    feat : 회원 가입 후 이메일 전송

--- 1Week_Mission/App/src/main/java/com/example/app/service/MemberService.java
+++ 1Week_Mission/App/src/main/java/com/example/app/service/MemberService.java
@@ -6,7 +6,11 @@ import com.example.app.domain.Member;
 import com.example.app.domain.status.MemberRole;
 import com.example.app.dto.request.JoinDto;
 import com.example.app.repository.MemberRepository;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,7 @@ public class MemberService {
 
   private final MemberRepository memberRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JavaMailSender javaMailSender;
 
   /*
   회원 가입
@@ -43,5 +48,23 @@ public class MemberService {
         .build();
 
     memberRepository.save(member);
+
+    sendEmail(member);
+  }
+
+  private void sendEmail(Member member) {
+    String subject = "[멋북스] 회원 가입해주셔서 감사합니다.";
+    String text = "안녕하세요. %s님! 멋북스에 오신 것을 환영합니다.<br/>".formatted(member.getNickname());
+
+    try {
+      MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
+      helper.setTo(member.getEmail());
+      helper.setSubject(subject);
+      helper.setText(text, true);
+      javaMailSender.send(mimeMessage);
+    } catch (MessagingException e) {
+      e.printStackTrace();
+    }
   }
 }
