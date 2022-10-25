e73485500619e12d37ae2c0fa4aca03b32f7aa7b
jyajoo <jyajoo1020@gmail.com>
2022-10-25 11:32:24 +0900

    add : order, orderItem entity

--- 2Week_Mission/App/src/main/java/com/ll/exam/final__2022_10_08/app/order/entity/Order.java
+++ 2Week_Mission/App/src/main/java/com/ll/exam/final__2022_10_08/app/order/entity/Order.java
@@ -1,5 +1,53 @@
 package com.ll.exam.final__2022_10_08.app.order.entity;
 
-public class Order {
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+
+import com.ll.exam.final__2022_10_08.app.base.entity.BaseEntity;
+import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/*
+주문
+- memberId, 회원 번호
+- payDate, 결제 날짜
+- readyStatus, 주문완료여부
+- isPaid, 결제완료여부
+- isCanceled, 취소여부
+- isRefunded, 환불여부
+- name, 주문명
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Order extends BaseEntity {
+
+  @ManyToOne(fetch = LAZY)
+  private Member member;
+
+  private LocalDateTime payDate;
+  private boolean readyStatus;
+  private boolean isPaid;
+  private boolean isCanceled;
+  private boolean isRefunded;
+  private String name;
+
+  @Builder.Default
+  @OneToMany(mappedBy = "order", cascade = ALL, orphanRemoval = true)
+  private List<OrderItem> orderItems = new ArrayList<>();
 
 }
---
+++ 2Week_Mission/App/src/main/java/com/ll/exam/final__2022_10_08/app/order/entity/OrderItem.java
@@ -0,0 +1,53 @@
+package com.ll.exam.final__2022_10_08.app.order.entity;
+
+import static javax.persistence.FetchType.LAZY;
+
+import com.ll.exam.final__2022_10_08.app.base.entity.BaseEntity;
+import com.ll.exam.final__2022_10_08.app.product.entity.Product;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/*
+주문 품목
+- orderId, 주문 번호
+- productId, 도서 번호
+- payDate, 결제 날짜
+- price, 가격
+- salePrice, 실제 판매가
+- wholesalePrice, 도매가
+- pgFree, 결제대행사 수수료
+- payPrice, 결제금액
+- refundPrice, 환불금액
+- isPaid, 결제여부
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class OrderItem extends BaseEntity {
+
+  @ManyToOne(fetch = LAZY)
+  @ToString.Exclude
+  private Order order;
+
+  @ManyToOne(fetch = LAZY)
+  private Product product;
+
+  private LocalDateTime payDate;
+  private int price;
+  private int salePrice;
+  private int wholesalePrice;
+  private int pgFee;
+  private int payPrice;
+  private int refundPrice;
+  private boolean isPaid;
+
+}
